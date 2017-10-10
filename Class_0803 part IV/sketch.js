0d15c97da86d38c00569241f6cec67a027b29726
AmanuelMebrahtu <31657123+AmanuelMebrahtu@users.noreply.github.com>
2017-10-09 22:26:16 -0400

    Add files via upload

--- Class_0803 part IV/sketch.js
+++ Class_0803 part IV/sketch.js
@@ -1,6 +1,6 @@
 function setup(){
 
-	createCanvas(600,600);
+	createCanvas(800,600);
 
 }
 
@@ -9,20 +9,31 @@ function draw(){
 	background(255);
 	
 var mapRot = map(mouseX, 0, width, 0, 2*PI);
+var mapRot1 = map(mouseX, 0, width, 0, 0.5*PI);
+var mapRot2 = map(mouseX, 0, width, 0, 1.33*PI);
 
 push();
+translate (100,300);
+scale(1.5);
 rotate(mapRot);
 rect(0,0,50,50);
 pop();
 
 push();
 
-translate(width/2, height/2);
-
-rotate(mapRot);
+translate(250, height/2);
+scale(2);
+rotate(mapRot1);
 rect(0, 0, 50, 50);
 
 
+pop();
+
+push();
+translate (500,300);
+scale(1.5);
+rotate(mapRot2);
+rect(0,0,50,50);
 pop();
 
 /*
